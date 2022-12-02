5205c3d6486e9bd38e40a6f308ed65b692be379d
unknown <klinkamama@gmail.com>
2022-12-02 19:11:46 +0300

    first commit

---
+++ prac9/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prac9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taskamanager.manager();
+        }
+    }
+}
---
+++ prac9/taskamanager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac9
+{
+    internal class taskamanager
+    {
+        public static int pos = 1;
+            public static Process[] procList = Process.GetProcesses();
+        public static void manager()
+        {
+            int maxlen_name = 0;
+            int notmaxlen_name = 0;
+            int yo = 1;
+            int maxlen_size = 0;
+            int notmaxlen_size = 0;
+
+            foreach (var proc in procList)
+            {
+                Console.SetCursorPosition(2, yo);
+                Console.Write(proc.ProcessName);
+                Console.SetCursorPosition(42, yo);
+                Console.Write(proc.PrivateMemorySize);
+                Console.SetCursorPosition(57, yo);
+                Console.Write(proc.BasePriority);
+                yo++;
+            }
+            while (arrows.key.Key != CoonsoleKey.Enter)
+            if (arrows.key.Key == ConsoleKey.UpArrow)
+                arrows.goup();
+            else if (arrows.key.Key == ConsoleKey.DownArrow)
+                arrows.godown();
+            if (arrows.key.Key != ConsoleKey.Enter)
+            Console.WriteLine("->");
+            arrows.key = Console.ReadKey();
+        }
+    }
+}
